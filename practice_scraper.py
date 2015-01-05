ea412674ef7b1ebc8aee33c3ec2ba9ba3d5983ea
Hilary Prichard <hilary.e.p@gmail.com>
2015-01-04 17:10:49 -0800

    basic web scraper

---
+++ practice_scraper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# 
+# Practice making a simple web scraper!
+# Scrapes the abstract titles from the LSA 2015 schedule
+# Print output intended to pipe to a text file
+
+from bs4 import BeautifulSoup
+from urllib2 import urlopen
+import re
+
+URL = "http://www.linguisticsociety.org/node/3901/schedule"
+
+def get_titles():
+    html = urlopen(URL).read()
+    soup = BeautifulSoup(html, "lxml")
+    titles = [a.string for a in soup.find_all(href=re.compile("abstract"))]
+    return titles
+
+if __name__ == '__main__':
+    raw = get_titles()
+    for x in raw:
+        y = x.encode('utf8')
+        print y + "\n"
